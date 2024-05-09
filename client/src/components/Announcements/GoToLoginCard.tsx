5e3b8bd87abb548a8855da6e57d172bb9b24485f
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-05-09 20:06:43 +0300

    redirect back to detail page after login

--- client/src/components/Announcements/GoToLoginCard.tsx
+++ client/src/components/Announcements/GoToLoginCard.tsx
@@ -1,14 +1,15 @@
-import { Button, Card, Col, Row, Stack } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
+import { Button, Card, Stack } from 'react-bootstrap'
+import { useLocation, useNavigate } from 'react-router-dom'
 
 export default function GoToLoginCard() {
   const navigate = useNavigate();
+  const location = useLocation();
 
   return (
     <Card className='mt-4'>
       <Stack direction='horizontal' gap={5}>
         Login to your CGS account or create a new account to contact this seller.
-        <Button className='btn-orange-dark' onClick={() => { navigate('/login') }}>
+        <Button className='btn-orange-dark' onClick={() => { navigate('/login', { state: { from: location }, replace: true }) }}>
           Log in / New account
         </Button>
       </Stack>

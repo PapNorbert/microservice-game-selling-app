cc3e460c51fe42e7f8ed129f7ab44897cde52f2c
Pap Norbert-Raymond <papnorbi4@gmail.com>
2024-06-21 16:29:13 +0300

    Exception handling

--- backend/orchestrator/src/main/java/edu/ubb/consolegamesales/backend/controller/exception/handler/MissingServletRequestParameterExceptionHandler.java
+++ backend/orchestrator/src/main/java/edu/ubb/consolegamesales/backend/controller/exception/handler/MissingServletRequestParameterExceptionHandler.java
@@ -4,8 +4,8 @@ import edu.ubb.consolegamesales.backend.model.ErrorData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,13 +14,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @Slf4j
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class DataIntegrityViolationExceptionHandler {
+public class MissingServletRequestParameterExceptionHandler {
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
+    @ExceptionHandler(MissingServletRequestParameterException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ResponseBody
-    public ErrorData handleFailedConstraintException(DataIntegrityViolationException exception) {
-        LOGGER.error("Invalid data " + exception.getMessage());
-        return new ErrorData("Invalid data");
+    public ErrorData handleFailedConstraintException(MissingServletRequestParameterException exception) {
+        LOGGER.error("Invalid request: {}", exception.getMessage());
+        return new ErrorData("Invalid request");
     }
 }
